b535a97cc4c0aaa822bd3ab24bc5203158132bce
Zach Newton <znewton@iastate.edu>
2017-11-03 14:37:23 -0500

    Stuff and things

---
+++ src/app/parties/parties.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parties',
+  templateUrl: './parties.component.html',
+  styleUrls: ['./parties.component.scss']
+})
+export class PartiesComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
