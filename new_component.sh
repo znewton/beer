7b4734062b3fcbbf6fed6387f88c1e06e781f7ee
znewton <znewton@iastate.edu>
2017-12-23 18:09:41 -0600

    new_component script modifications

--- new_component.sh
+++ new_component.sh
@@ -5,8 +5,8 @@ if [ -z $CNAME ]; then
     echo "Component Name required."
     exit
 fi
-cd ./lib/src
-DIR="./src"
+cd ./src
+DIR="./"
 COMP_DIR="$DIR/$CNAME"
 if [ -d "$COMP_DIR" ]; then
     echo ""
@@ -18,9 +18,9 @@ if [ -d "$COMP_DIR" ]; then
     rm -rf "$COMP_DIR"
 fi
 mkdir "$COMP_DIR"
-JS="$COMP_DIR/$CNAME.jsx"
-touch "$JS"
-CSS="$COMP_DIR/$CNAME.scss"
+JSX="$COMP_DIR/$CNAME.jsx"
+touch "$JSX"
+SCSS="$COMP_DIR/$CNAME.scss"
 touch "$CSS"
 TEST="$COMP_DIR/$CNAME.test.js"
 touch "$TEST"
@@ -29,7 +29,7 @@ LOWER_CNAME=$(echo "$CNAME" | awk '{print tolower($0)}')
 FIRST_LOWER_CNAME="$(echo ${CNAME:0:1} | tr '[:upper:]' '[:lower:]')${CNAME:1}"
 SNAKE_CNAME=$(echo "$FIRST_LOWER_CNAME" | sed 's/\([A-Z]\)/-\1/g' | awk '{print tolower($0)}')
 
-cat > "$JS" << JS
+cat > "$JSX" << JSX
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './${CNAME}.css';
@@ -44,12 +44,14 @@ class ${CNAME} extends Component {
 }
 
 export default ${CNAME};
-JS
 
-cat > "$CSS" << CSS
+JSX
+
+cat > "$SCSS" << SCSS
 .${SNAKE_CNAME} {
 }
-CSS
+
+SCSS
 
 cat > "$TEST" << JS
 import React from 'react';
@@ -63,4 +65,4 @@ it('renders without crashing', () => {
 
 JS
 
-cd ../..
+cd ..
